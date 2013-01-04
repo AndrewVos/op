97a0fdd7e3256079eb0ef09c2bc64eb5a362a689
Andrew Vos <andrew.vos@gmail.com>
2013-01-04 09:47:11 +0000

    spike code

---
+++ spike/test.rb
@@ -0,0 +1,14 @@
+system "ruby file.rb generate -o hello"
+puts
+
+system "ruby file.rb generate -ro hello"
+puts
+
+system "ruby file.rb generate --random --output hello"
+puts
+
+system "ruby file.rb generate -or"
+puts
+
+system "ruby file.rb deploy"
+puts
